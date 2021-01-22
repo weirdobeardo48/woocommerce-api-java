68e0724c424c66e3fd421e5c58c9767e0521de2b
weirdobeardo48 <truongnx@easyshoppingfeed.com>
2021-01-22 10:14:40 +0700

    20210122 | Fix wrong import

--- src/main/java/woocommerce/entity/product/ProductAttribute.java
+++ src/main/java/woocommerce/entity/product/ProductAttribute.java
@@ -1,7 +1,5 @@
 package woocommerce.entity.product;
 
-import com.sun.org.apache.xpath.internal.operations.String;
-
 import java.util.List;
 
 /**
@@ -15,4 +13,64 @@ public class ProductAttribute {
     private boolean visible;
     private boolean variation;
     private List<String> options;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
+
+    public boolean isVisible() {
+        return visible;
+    }
+
+    public void setVisible(boolean visible) {
+        this.visible = visible;
+    }
+
+    public boolean isVariation() {
+        return variation;
+    }
+
+    public void setVariation(boolean variation) {
+        this.variation = variation;
+    }
+
+    public List<String> getOptions() {
+        return options;
+    }
+
+    public void setOptions(List<String> options) {
+        this.options = options;
+    }
+
+    @Override
+    public String toString() {
+        return "ProductAttribute{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", position=" + position +
+                ", visible=" + visible +
+                ", variation=" + variation +
+                ", options=" + options +
+                '}';
+    }
 }
